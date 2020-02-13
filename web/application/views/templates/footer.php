5906c36a69c75197475030131c35accde83f7e49
mirzaRakha28 <mirzarakhadwipradana28@gmail.com>
2020-02-13 17:27:10 +0700

    menambahkan footer dan header

---
+++ web/application/views/templates/footer.php
@@ -0,0 +1,30 @@
+<footer>
+        <footer>
+            <div class="row">
+                <div class="col-sm-6 col-md-4 footer-navigation">
+                    <h3></h3><img src="logo1.png" style="width: 50px;min-width: 50px;" /><img src="logo2.png" style="width: 50px;min-width: 50px;margin: 14px;" />
+                    <p class="links"><a href="#">Beranda</a><strong> · </strong><a href="#">Galeri</a><strong> · </strong><a href="#">Produk Dan Jasa</a><strong> · </strong><a href="#">Karir</a><strong> · </strong><a href="#">About</a><strong></strong></p>
+                </div>
+                <div class="col-sm-6 col-md-4 footer-contacts">
+                    <div><span class="fa fa-map-marker footer-contacts-icon"></span>
+                        <p><span class="new-line-span">Jalan Kebayoran Lama Raya No.30 A<br /></span> Jakarta Selatan, Indonesia</p>
+                    </div>
+                    <div><i class="fa fa-phone footer-contacts-icon"></i>
+                        <p class="footer-center-info email text-left">(021) 548 1672<br /></p>
+                    </div>
+                    <div><i class="fa fa-envelope footer-contacts-icon"></i>
+                        <p><a href="#" target="_blank">marketing@ekselensi.co.id,<br /></a></p>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+                <div class="col-md-4 footer-about">
+                    <h4>About the company</h4>
+                    <p> Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet. </p>
+                    <div class="social-links social-icons"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a><a href="#"><i class="fa fa-github"></i></a></div>
+                </div>
+            </div>
+        </footer>
+    </footer>
+</body>
+
+</html>
---
+++ web/application/views/templates/header.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+    <title>ekselensi</title>
+    <link rel="stylesheet" href="bootstrap.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kaushan+Script" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" />
+    <link rel="stylesheet" href="./embed/fonts/fontawesome5-overrides.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400|Roboto:300,400,700" />
+    <link rel="stylesheet" href="<?= base_url()?>Articale-List-With-Image-Zoom.css" />
+    <link rel="stylesheet" href="<?= base_url()?>dh-row-titile-text-image-right-1.css" />
+    <link rel="stylesheet" href="<?= base_url()?>Good-login-dropdown-menu-1.css" />
+    <link rel="stylesheet" href="<?= base_url()?>Good-login-dropdown-menu.css" />
+    <link rel="stylesheet" href="<?= base_url()?>Google-Style-Login.css" />
+    <link rel="stylesheet" href="<?= base_url()?>Job-management.css" />
+    <link rel="stylesheet" href="<?= base_url()?>Pretty-Footer.css" />
+    <link rel="stylesheet" href="<?= base_url()?>sticky-dark-top-nav-with-dropdown.css" />
+    <link rel="stylesheet" href="<?= base_url()?>Swipe-Slider-7.css" />
+    <link rel="stylesheet" href="<?= base_url()?>User-Payment-Overview-Rows---Panel-Container.css" />
+</head>
+<body>
