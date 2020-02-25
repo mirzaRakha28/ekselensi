60a02304e17376a8148ed3dd853becbb7d4019f5
emryswarlock <ardha275@gmail.com>
2020-02-26 01:21:49 +0700

    fixing some merge conflig of wa button

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,8 +1,18 @@
-<a href=
+<!-- <a href=
     "https://wa.me/62882210714854?text=
         <?= " Hello There !, I was interest to your product , Can you contact me ? because i wanna know more about yout product.^^" ?>" 
         target="_blank" class="cd-top  cd-bottom--fade-out cd-top--show" style="background-image: url(&quot;<?= base_url()?>assets/img/wa.png &quot;);background-size: contain;background-color: rgba(232,98,86,0);">
-</a>   
+</a>    -->
+
+<a class="d-md-flex justify-content-end" id="wp1" target="_blank" style="font-size: 30px;margin-bottom: 54px;margin-right: -1px;width: 132px;height: 45px;background-color: rgb(25,230,21);">       
+        <p style="font-size: 17px;margin-top: 9px;margin-right: 21px;font-family: Montserrat, sans-serif;">Sandi</p><img class="border rounded-circle" style="width: 45px;height: 45px;" src="<?= base_url()?>assets/img/team/2.jpg"></a>
+        <a class="d-md-flex justify-content-end" id="wp2" target="_blank" style="font-size: 30px;margin-bottom: 110px;margin-right: -1px;width: 132px;height: 45px;background-color: rgb(25,230,21);">
+        <p style="font-size: 17px;margin-top: 9px;margin-right: 21px;font-family: Montserrat, sans-serif;">Sandi</p><img class="border rounded-circle" style="width: 45px;height: 45px;" src="<?= base_url()?>assets/img/team/2.jpg"></a>
+        <a class="d-md-flex justify-content-end" id="wp3" target="_blank" style="font-size: 30px;margin-bottom: 164px;margin-right: -1px;width: 132px;height: 45px;background-color: rgb(25,230,21);">
+        <p style="font-size: 17px;margin-top: 9px;margin-right: 21px;font-family: Montserrat, sans-serif;">Sandi</p><img class="border rounded-circle" style="width: 45px;height: 45px;" src="<?= base_url()?>assets/img/team/2.jpg"></a>
+
+        <a class="cd-top  cd-bottom--fade-out cd-top--show" id="wa"  onClick = "myMove()" style="background-image: url(&quot;<?= base_url()?>assets/img/wa.png&quot;);background-size: contain;background-color: rgba(232,98,86,0);background-position: top;"></a>
+
 
 
 <footer>
@@ -58,6 +68,73 @@
     <script src="<?= base_url()?>assets/js/currency.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script>
+        var wp1 = document.getElementById("wp1");
+        var wp2 =document.getElementById("wp2");
+        var wp3 = document.getElementById("wp3");
+        var opp =  document.getElementById("wp3").style.opacity;
+        
+        
+        function showContact(){
+            var opp =  document.getElementById("wp3").style.opacity;
+            var id = setInterval(opacityZero,20);
+            function opacityZero(){
+                if(wp1.style.opacity == 1){
+                    clearInterval(id);
+                }
+                else{
+                    opp = opp + 0.5;
+                    wp1.style.opacity = opp ;
+                    wp2.style.opacity = opp;
+                    wp3.style.opacity = opp;
+                }
+            }
+            
+            
+            }
+            function myMove() {
+                    if(wp1.style.opacity == 0){
+                        
+                        wp1.style.opacity = 1;
+                        wp2.style.opacity = 1;
+                        wp3.style.opacity = 1;
+                        var pos = 0;
+                        var id = setInterval(frame, 3);
+                        function frame() {
+                            if (pos == 40) {
+                            clearInterval(id);
+                            } else {
+                            pos++; 
+                            wp1.style.bottom = pos + 'px'; 
+                            wp2.style.bottom = pos + 'px'; 
+                            wp3.style.bottom = pos + 'px'; 
+                            }
+                        }
+    
+                    }
+                    else{
+                        var pos = 40;
+                        var id = setInterval(frame, 5);
+                        function frame() {
+                            if (pos == 0) {
+                            clearInterval(id);
+                            } else {
+                            pos--; 
+                            wp1.style.bottom = pos + 'px'; 
+                            wp2.style.bottom = pos + 'px'; 
+                            wp3.style.bottom = pos + 'px'; 
+                            }
+                        wp1.style.opacity = 0;
+                        wp2.style.opacity = 0;
+                        wp3.style.opacity = 0;
+                        }
+                    }
+                
+                
+                }
+        
+    </script>
+
     
     
 </body>
