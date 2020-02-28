074788896d6ccaec43c236b3c25042bacd9092a2
mirzaRakha28 <54165216+mirzaRakha28@users.noreply.github.com>
2020-02-29 00:37:21 +0700

    Update footer_admin.php

--- application/views/templates/footer_admin.php
+++ application/views/templates/footer_admin.php
@@ -1,9 +1,9 @@
-</div>
-    <!-- <script src="<?= base_url()?>assets_admin/js/jquery.min.js"></script> -->
-    <!-- <script
+</div>    
+<script src="<?= base_url()?>assets_admin/js/jquery.min.js"></script>
+    <script
   src="https://code.jquery.com/jquery-3.4.1.min.js"
   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-  crossorigin="anonymous"></script> -->
+  crossorigin="anonymous"></script>
     <script src="<?= base_url()?>assets_admin/bootstrap/js/bootstrap.min.js"></script>
     <script src="<?= base_url()?>assets_admin/js/Sidebar-Menu.js"></script>
 </body>
